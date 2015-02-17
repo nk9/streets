7d08bdf46021c0311d4fcec468ad88ac4aef43d2
Nick Kocharhook <nick@kocharhook.com>
2015-02-17 20:53:38 +0000

    Add some relevant types (Close, etc). Add main(). Remove Upper|Lower for Upper Street.

--- add-name-bases.py
+++ add-name-bases.py
@@ -12,48 +12,63 @@ def ExtractNameBase(name):
 	"""
 	name = re.sub('[.]$','',name)
 
-	if not re.match('^[A-Za-z0-9 ]+$',name):
+	if not re.match('^[A-Za-z0-9\.\' ]+$',name):
 		return ''
 
 	name = re.sub('^[NSEWnsew]{1,2}[.]? ','',name)
 	name = re.sub(' [NSEWnsew]{1,2}[.]?$','',name)
 	name = re.sub('^(North|South|East|West|Northwest|Northeast|Southwest|Southeast) ','',name)
 	name = re.sub(' (North|South|East|West|Northwest|Northeast|Southwest|Southeast)$','',name)
-	name = re.sub('^(Upper|Lower) ','',name)
+	# name = re.sub('^(Upper|Lower) ','',name)
 	name = re.sub('^The ','',name)
 
-	if not re.search(' (Street|Drive|Way|Lane|Road|Freeway|Highway|Expressway|Alley|Terrace|Court|Boulevard|Avenue|Place|Skyway|Parkway|Square|Plaza|Park|Bridge|Tollway|Circle|Loop|St|Dr|Ln|Rd|Fwy|Expy|Hwy|Ter|Ct|Blvd|Ave|Pl|Pky|Pkwy|Sq|Br|Tunnel|Row|Gardens|Trail)$',name):
-		return ''
+	streetTypes = [
+		"Street", "Drive","Way","Lane","Road","Freeway","Highway","Expressway","Alley",
+		"Terrace","Court","Boulevard","Avenue","Place","Skyway","Parkway","Square","Plaza",
+		"Park","Bridge","Crescent","Pike","Mall","Tollway","Circle","Loop","Walk",
+		"Tunnel","Row","Gardens","Trail","Grove","Mews","Close","Crescent","Rise","Yard","Villas",
+		"St","Dr","Ln","Rd","Fwy","Expy","Hwy","Ter","Ct","Blvd","Ave","Pl","Pky","Pkwy","Sq","Br",
+	]
+	allTypesREString = ' ({})$'.format("|".join(streetTypes))
 
-	return re.sub(' (Street|Drive|Way|Lane|Road|Freeway|Highway|Expressway|Alley|Terrace|Court|Boulevard|Avenue|Place|Skyway|Parkway|Square|Plaza|Park|Bridge|Tollway|Circle|Loop|St|Dr|Ln|Rd|Fwy|Expy|Hwy|Ter|Ct|Blvd|Ave|Pl|Pky|Pkwy|Sq|Br|Tunnel|Row|Gardens|Trail)$','',name)
+	if not re.search(allTypesREString,name):
+			return ''
 
-if len(sys.argv) < 4:
-	print "Invalid syntax, need DB user, DB password, and DB name"
-else:
-	db_user = sys.argv[1]
-	db_password = sys.argv[2]
-	db_name = sys.argv[3]
-	db = MySQLdb.connect(host="localhost", user=db_user, passwd=db_password, db=db_name)	
-	cur = db.cursor()
-	update_cur = db.cursor()
+	return re.sub(allTypesREString, '', name)
 
-	cur.execute("SELECT `ITEM_ID`,TRIM(`NAME`) FROM `" + db_name + "`.`STREET_DETAILS` WHERE TRIM(`NAME_BASE`) = ''")
+def main():
+	if len(sys.argv) < 4:
+		print "Invalid syntax, need DB user, DB password, and DB name"
+	else:
+		db_user = sys.argv[1]
+		db_password = sys.argv[2]
+		db_name = sys.argv[3]
+		db = MySQLdb.connect(host="localhost", user=db_user, passwd=db_password, db=db_name)
+		cur = db.cursor()
+		update_cur = db.cursor()
 
-	print str(cur.rowcount)+" streets with no name base"
+		cur.execute("SELECT `ITEM_ID`,TRIM(`NAME`) FROM `" + db_name + "`.`STREET_DETAILS` WHERE TRIM(`NAME_BASE`) = ''")
 
-	rowcount = cur.rowcount
-	start_time = time.time()
-	for i in range(rowcount):
-		
-		row = cur.fetchone()
-		street_id = str(row[0])
+		print str(cur.rowcount)+" streets with no name base"
 
-		street_name = str(row[1])
+		rowcount = cur.rowcount
+		start_time = time.time()
+		for i in range(rowcount):
+			
+			row = cur.fetchone()
+			street_id = str(row[0])
 
-		new_base = ExtractNameBase(street_name);
-				
-		update_cur.execute("UPDATE `"+db_name+"`.`STREET_DETAILS` SET `NAME_BASE`=%s WHERE `ITEM_ID`=%s",(new_base,street_id))
-		db.commit()
-		execution_time = round(time.time()-start_time)		
-		if len(new_base) == 0:
-			print "("+str(i+1)+"/"+str(rowcount)+") ["+str(int(math.floor(execution_time / 60)))+":"+str(int(execution_time % 60))+"] "+"Could not compute name base for "+street_name
+			street_name = str(row[1])
+
+			new_base = ExtractNameBase(street_name);
+					
+			update_cur.execute("UPDATE `"+db_name+"`.`STREET_DETAILS` SET `NAME_BASE`=%s WHERE `ITEM_ID`=%s",(new_base,street_id))
+			db.commit()
+			execution_time = round(time.time()-start_time)
+			if len(new_base) == 0:
+				print "("+str(i+1)+"/"+str(rowcount)+") ["+str(int(math.floor(execution_time / 60)))+":"+str(int(execution_time % 60))+"] "+"Could not compute name base for "+street_name
+
+
+# Default function is main()
+if __name__ == '__main__':
+	main()
